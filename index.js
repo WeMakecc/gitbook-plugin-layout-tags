e892539f3df943c440680f4af4784eade80846be
CNTLSN <aless.contini@gmail.com>
2018-01-17 17:55:37 +0100

    add double column tag

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ module.exports = {
     blocks: {
         image: {
             process: function(block) {
-                return "<img src='"+block.body+"' alt='"+block.args[0]+"' width='"+block.args[1]+"' align='"+block.args[2]+"' style='"+block.args[2]+"'>";
+                return "<img src='"+block.body+"' alt='"+block.args[0]+"' width='"+block.args[1]+"' align='"+block.args[2]+"' style='"+block.args[3]+"'>";
             }
+        },
+        double-column: {
+            blocks: ['col'],
+            process: function(parentBlock) {
+              var columns = '';
+              console.log(parentBlock.blocks);
+              parentBlock.blocks.forEach((block) => {
+                columns += `<div style="flex: ${block.args[0]};">${block.body}</div>`;
+              });
+
+              return `<div style="display: flex;">${columns}</div>`;
+            }
+          }
         }
     }
 };
