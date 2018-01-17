1c18455d28f26331182addbf65c73ed15cafcf34
CNTLSN <aless.contini@gmail.com>
2018-01-17 18:18:12 +0100

    test fix grid layout tag

--- index.js
+++ index.js
@@ -5,28 +5,16 @@ module.exports = {
                 return "<img src='"+block.body+"' alt='"+block.args[0]+"' width='"+block.args[1]+"' align='"+block.args[2]+"' style='"+block.args[3]+"'>";
             }
         },
-        double-column: {
-            // blocks: ['col'],
-            // process: function(parentBlock) {
-            //   var columns = '';
-            //   console.log(parentBlock.blocks);
-            //   parentBlock.blocks.forEach((block) => {
-            //     columns += `<div style="flex: ${block.args[0]};">${block.body}</div>`;
-            //   });
+        grid: {
+            blocks: ['col'],
+            process: function(parentBlock) {
+                var columns = '';
+                console.log(parentBlock.blocks);
+                parentBlock.blocks.forEach((block) => {
+                  columns += `<div style="flex: ${block.args[0]};">${block.body}</div>`;
+                });
 
-            //   return `<div style="display: flex;">${columns}</div>`;
-            // }
-            grid: {
-                blocks: ['col'],
-                process: function(parentBlock) {
-                    var columns = '';
-                    console.log(parentBlock.blocks);
-                    parentBlock.blocks.forEach((block) => {
-                      columns += `<div style="flex: ${block.args[0]};">${block.body}</div>`;
-                    });
-
-                    return `<div style="display: flex;">${columns}</div>`;
-                }
+                return `<div style="display: flex;">${columns}</div>`;
             }
         }
     }
