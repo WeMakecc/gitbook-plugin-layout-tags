6893f2d412a4f80c941f501efc4004a08e900c7d
CNTLSN <aless.contini@gmail.com>
2018-01-22 12:20:10 +0100

    test double column for pdf

--- index.js
+++ index.js
@@ -8,16 +8,22 @@ module.exports = {
               return "<p style='overflow:hidden'>"+img_tag+""+text_tag+"</p>";
             }
         },
-        grid: {
-            blocks: ['col'],
-            process: function(parentBlock) {
-                var columns = '';
-                console.log(parentBlock.blocks);
-                parentBlock.blocks.forEach((block) => {
-                  columns += `<div style="flex: ${block.args[0]};">${block.body}</div>`;
-                });
+        // grid: {
+        //     blocks: ['col'],
+        //     process: function(parentBlock) {
+        //         var columns = '';
+        //         console.log(parentBlock.blocks);
+        //         parentBlock.blocks.forEach((block) => {
+        //           columns += `<div style="flex: ${block.args[0]};">${block.body}</div>`;
+        //         });
 
-                return `<div style="display: flex;">${columns}</div>`;
+        //         return `<div style="display: flex;">${columns}</div>`;
+        //     }
+        // }
+        grid: {
+            blocks: ['col 1'], ['col 2'],
+            process: function(parentBlock) {
+                return `<div style="align: left; width: 50%;">`+parentBlock.blocks[0].body+`</div><div style="align: left; width: 50%;">`+parentBlock.blocks[1].body+`</div>`;
             }
         }
     }
