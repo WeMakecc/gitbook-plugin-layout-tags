e92774714382376c978fb73f1bf898f952bcc81e
CNTLSN <aless.contini@gmail.com>
2018-01-22 11:00:36 +0100

    comment out image tag

--- index.js
+++ index.js
@@ -5,23 +5,23 @@ module.exports = {
         //         return "<p style='overflow:hidden'><img src='"+block.body+"' alt='"+block.args[0]+"' width='"+block.args[1]+"' align='"+block.args[2]+"' style='"+block.args[3]+"'></p>";
         //     }
         // },
-        image: {
-          blocks: {
-            image_url {
-              process: function(block) {
-                  var img_tag = "<img src='"+block.body+"' alt='"+block.args[0]+"' width='"+block.args[1]+"' align='"+block.args[2]+"' style='"+block.args[3]+"'>";
-              }
-            },
-            image_text {
-              process: function(block) {
-                  var text_tag = block.body;
-              }
-            }
-          }
-            process: function(block) {
-                return "<p style='overflow:hidden'>"+img_tag+""+text_tag+"</p>";
-            }
-        },
+        // image: {
+        //   blocks: {
+        //     image_url {
+        //       process: function(block) {
+        //           var img_tag = "<img src='"+block.body+"' alt='"+block.args[0]+"' width='"+block.args[1]+"' align='"+block.args[2]+"' style='"+block.args[3]+"'>";
+        //       }
+        //     },
+        //     image_text {
+        //       process: function(block) {
+        //           var text_tag = block.body;
+        //       }
+        //     }
+        //   }
+        //     process: function(parentBlock) {
+        //         return "<p style='overflow:hidden'>"+img_tag+""+text_tag+"</p>";
+        //     }
+        // },
         grid: {
             blocks: ['col'],
             process: function(parentBlock) {
