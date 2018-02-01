dad5cbe6e8e6d255798decebbec62fdc51cd77dc
CNTLSN <aless.contini@gmail.com>
2018-02-01 15:59:02 +0100

    add alignment for bom text elements

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ module.exports = {
             blocks: ['bom_image', 'bom_description', 'bom_quantity'],
             process: function(parentBlock) {
               var img_tag = "<img src='"+parentBlock.blocks[0].body+"' alt='"+parentBlock.blocks[0].args[0]+"' width='"+parentBlock.blocks[0].args[1]+"' align='"+parentBlock.blocks[0].args[2]+"' style='"+parentBlock.blocks[0].args[3]+"'>";
-              var description_tag = parentBlock.blocks[1].body;
-              var quantity_tag = parentBlock.blocks[2].body;
+              var description_tag = "<p style='float: left'>"+parentBlock.blocks[1].body+"</p>";
+              var quantity_tag =  "<p style='float: right'>"+parentBlock.blocks[2].body+"</p>";
               return "<p style='overflow:hidden'>"+img_tag+""+description_tag+""+quantity_tag+"</p>";
             }
         },
